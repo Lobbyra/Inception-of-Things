1718fd6b60ac63284fb12a770583a0b458c81f9d
joe <lobbyra>
2023-08-01 07:22:55 -0400

    finish p2

--- common/install_vagrant.sh
+++ common/install_vagrant.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-curl -O https://releases.hashicorp.com/vagrant/2.2.6/vagrant_2.2.6_x86_64.deb
-sudo apt update
-sudo apt install ./vagrant_2.2.6_x86_64.deb
-vagrant --version
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install vagrant
--- p2/scripts/masterEntrypoint.sh
+++ p2/scripts/masterEntrypoint.sh
@@ -12,3 +12,9 @@ curl -sfL https://get.k3s.io | sudo sh -
 sudo cp /tmp/hostConfs/k3s.service /etc/systemd/system/k3s.service
 sudo systemctl daemon-reload
 sudo systemctl restart k3s
+
+sudo k3s kubectl apply -f /tmp/hostConfs/app.dep.yaml
+sudo k3s kubectl apply -f /tmp/hostConfs/app.svc.yaml
+sudo k3s kubectl apply -f /tmp/hostConfs/ingress.yaml
+
+echo "192.168.56.110 app1.com app2.com app3.com" | sudo tee -a /etc/hosts
